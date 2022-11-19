da272adad79446b46a09a5e492da92e1dd040467
rpp293 <66886441+rpp293@users.noreply.github.com>
2022-11-19 16:48:41 -0600

    Update scriptbackup.js
    Signed-off-by: rpp293 <66886441+rpp293@users.noreply.github.com>

--- scriptbackup.js
+++ scriptbackup.js
@@ -15,7 +15,7 @@ d3.csv("SiteIT.csv").then(function (data) {
     d3.selectAll("p").classed('noresults', true).html("")
     d3.event.preventDefault();
     var inputElement = d3.select("#user-input");
-    var inputValue = inputElement.property("value").toUpperCase();
+    var inputValue = inputElement.property("value").toUpperCase().trim();
 
     // console.log(inputValue.length);
     // console.log(movies);
@@ -23,22 +23,16 @@ d3.csv("SiteIT.csv").then(function (data) {
       d3.select("p").classed('noresults2', true).html("<center><strong>Please try using at least 2 characters to search for the state</strong>")
       inputValue = "Something to give no results"
     }
-    var filteredData = SiteIT.filter(SiteIT => SiteIT.State.toUpperCase().includes(inputValue));
+    var filteredData = SiteIT.filter(SiteIT => SiteIT.State.toUpperCase().trim().includes(inputValue));
     // console.log(filteredData.length)
     if (filteredData.length === 0 && inputValue !== "Something to give no results"){
       d3.select("p").classed('noresults', true).html("<center><strong>No results. Please check your spelling!</strong>")
     }
    
-    output = '';
+    output = _.sortBy(filteredData, 'State')
     
-    for (var i = 0; i < filteredData.length; i++) {
-      d3.select("tbody").insert("tr").html(
-        "<td>" + [i + 1] + "</td>" +
-        "<td>" + (output[i]['State']) + "</td>" +
-        "<td>" + (output[i]['Zone']) + "</td>" +
-        "<td>" + (output[i]['PrimaryRep']) + "</td>" +
-        "<td>" + (output[i]['SecondaryRep']) + "</td>" +
-        "<td>" + (output[i]['Supervisor']) + "</td") }
+     for (var i = 0; i < filteredData.length; i++) {
+      d3.select("tbody").insert("tr").html("<td>"+[i+1]+"</td>"+"<td>" +(output[i]['State'])+"</td>" +"<td>" +(output[i]['Zone'])+"</td>"  +"<td>" +(output[i]['PrimaryRep'])+"</td>"+"<td>" +(output[i]['SecondaryRep'])+"</td>") +(output[i]['Supervisor'])+"</td>")}
     };
   
   window.resizeTo(screen.width,screen.height)
