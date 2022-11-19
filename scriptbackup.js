68f488a0bedcda1fdf06fcc3b51e0849475f3d2c
rpp293 <66886441+rpp293@users.noreply.github.com>
2022-11-19 15:13:34 -0600

    Update scriptbackup.js
    Signed-off-by: rpp293 <66886441+rpp293@users.noreply.github.com>

--- scriptbackup.js
+++ scriptbackup.js
@@ -32,8 +32,8 @@ d3.csv("SiteIT.csv").then(function (data) {
     for (var i = 0; i < filteredData.length; i++) {
     d3.select("tbody").insert("tr").html(
     "<td>" + [i+1] + "</td>" +
-    "<td>" + (output[i]['State'])+"</a>"+"</td>" + 
-	"<td>" + (output[i]['Zone'])+"</a>"+"</td>" + 
+    "<td>" + (output[i]['State'])+"</td>" + 
+     "<td>" + (output[i]['Zone'])+"</td>" + 
     "<td>" + (output[i]['PrimaryRep'])+"</td>" +
     "<td>" + (output[i]['SecondaryRep'])+"</td>" +
     "<td>" + (output[i]['Supervisor'])+"</td" ) }
